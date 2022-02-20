8e3f2fe0552efbc73eb06c42e447a7d4b68f7545
ms-jagadeeshan <jagadeeshanmsj@gmail.com>
2022-02-21 01:19:04 +0530

    creating config.json if not exists

--- src/config.cpp
+++ src/config.cpp
@@ -10,7 +10,6 @@
  */
 #include "config.h"
 
-
 void json2config(nlohmann::json &j, struct config_t &conf)
 {
     j.at("chat_id").get_to(conf.chat_id);
@@ -27,12 +26,39 @@ void get_config(struct config_t &conf)
     std::ifstream conf_in(CONFIG_PATH);
     if (conf_in.is_open())
     {
+        conf_in.close();
+        std::ofstream conf_out(CONFIG_PATH);
+        if (conf_out.is_open())
+        {
+            conf_out << R"V0G0N({
+    "chat_id": "",
+    "token": "",
+    "repos": [
+        "ms-jagadeeshan/telegram_bot-github-release-tracker",
+        "torvalds/linux"
+    ],
+    "github_personal_token": ""
+}
+)V0G0N";
+        }
+        else
+        {
+            std::cerr << "telgram-bot : cannot access \'" << CONFIG_PATH << "\' : " << strerror(errno) << '\n';
+            exit(1);
+        }
+        conf_out.close();
+    }
+    conf.repos.clear();
+    if (conf_in.is_open())
+    {
         nlohmann::json config_json;
         conf_in >> config_json;
         json2config(config_json, conf);
+        conf_in.close();
     }
     else
     {
+        conf_in.close();
         std::cerr << "telgram-bot : cannot access \'" << CONFIG_PATH << "\' : " << strerror(errno) << '\n';
         exit(1);
     }
